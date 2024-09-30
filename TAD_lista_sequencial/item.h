3a7065343515d5957c6318257d8c9acc44a4563c
Johana Pizarro <102596180+JohanaPizarroL@users.noreply.github.com>
2024-09-29 21:05:41 -0300

    inferface item

--- TAD_lista_sequencial/item.h
+++ TAD_lista_sequencial/item.h
@@ -5,10 +5,10 @@
 
 	typedef struct item_ ITEM;
 
-	ITEM *item_criar(int chave);
-	bool item_apagar(ITEM **item);
-	void item_imprimir(ITEM *item);
-	int item_get_chave(ITEM *item);
-	bool item_set_chave(ITEM *item, int chave);
+	ITEM *item_criar (int chave, void *comp);
+	bool item_apagar (ITEM **item);
+	int item_get_chave (ITEM *item);
+	bool item_set_chave (ITEM *item, int chave);
+	void *item_get_dados (ITEM *item);
 
 #endif
